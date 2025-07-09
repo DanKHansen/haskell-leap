3ff3874499670734979d8b446cb45c9d6bf22de7
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-09 09:10:47 +0200

    refactor

--- src/LeapYear.hs
+++ src/LeapYear.hs
@@ -2,7 +2,8 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year =
-  case (rem year 4 == 0, rem year 100 == 0, rem year 400 == 0) of
-    (True, False, _) -> True
-    (True, True, True) -> True
+  case (rem year 4, rem year 100, rem year 400) of
+    (0, 0, 0) -> True
+    (0, 0, _) -> False
+    (0, _, _) -> True
     _ -> False
